d12558bdfdf3e2bcf120fe54fa9c868029f20b4b
sinatriiobimo <sinatriobimbim@gmail.com>
2021-05-31 01:33:23 +0700

    'final'

--- coba.py
+++ coba.py
@@ -12,17 +12,6 @@ with open(IMG_PATH, "rb") as image2string:
 print(encoded_img)
 print(type(encoded_img))
 
-# base64_decode = base64.b64decode(encoded_img)
-
-# print(type(base64_decode))
-# # img = image.load_img(io.BytesIO(base64_decode))
-# img = Image.open(io.BytesIO(base64_decode))
-# print(type(img))
-# print(img.size)
-# img_resize = img.resize((224, 224), Image.ANTIALIAS)
-# print(img_resize.size)
-# img_resize.show()
-# # im
 model = Model()
 
 prediction= model.predict(encoded_img)
